27c163d534027434ee8ecc1ec61fa9fc2e42b988
Your Name <you@example.com>
2025-07-25 10:02:46 -0700

    Edaurdo

---
+++ src/app/core/interceptors/api.interceptor.ts
@@ -0,0 +1,8 @@
+import { HttpInterceptorFn } from "@angular/common/http";
+
+export const apiInterceptor: HttpInterceptorFn = (req, next) => {
+  const apiReq = req.clone({
+    url: `https://api.realworld.build/api${req.url}`,
+  });
+  return next(apiReq);
+};
